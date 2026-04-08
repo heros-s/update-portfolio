1f0034261c7a84e70d0debdb82549fbffd1beff7
Heros Dinão <herosdefreitas@gmail.com>
2026-04-08 02:05:18 -0300

    feat: add GA

--- portfolio-react/app/layout.tsx
+++ portfolio-react/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/next';
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -9,6 +10,18 @@ export default function RootLayout({
     <html suppressHydrationWarning>
       <body>
         {children}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-6Y3690QMNW`} strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6Y3690QMNW');
+          `}
+        </Script>
         <Analytics />
       </body>
     </html>
